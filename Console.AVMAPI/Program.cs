32bed2c00d1b21d1aa4393a242d3089cfd6a541d
Gerhard Ahrens <Gerhard.Ahrens@lifeprojects.de>
2026-07-13 09:45:01 +0200

    Projektdateien hinzufügen.

---
+++ Console.AVMAPI/Program.cs
@@ -0,0 +1,59 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Lifeprojects.de">
+//     Class: Program
+//     Copyright © Lifeprojects.de 2026
+// </copyright>
+// <Template>
+// 	Version 3.0.2026.2, 15.04.2026
+// </Template>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>13.07.2026 09:36:54</date>
+//
+// <summary>
+// Konsolen Applikation mit der der Zugriff auf eine Fritzbox 7590 demonstriert werden soll
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace Console.AVMAPI
+{
+    /* Imports from NET Framework */
+    using System;
+
+    public class Program
+    {
+        public Program()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.CursorVisible = false;
+        }
+
+        private static void Main(string[] args)
+        {
+            CMenu mainMenu = new CMenu("Fritz 7590 Menü");
+            mainMenu.AddItem("Verbindung zur Fritz-Box herstellen", MenuPoint1);
+            mainMenu.AddItem("Beenden", () => ApplicationExit());
+            mainMenu.Show();
+        }
+
+        private static void ApplicationExit()
+        {
+            Environment.Exit(0);
+        }
+
+        private static void MenuPoint1()
+        {
+            Console.Clear();
+
+            Console.Wait();
+        }
+
+        private static void UnterMenuPoint(string param)
+        {
+            Console.Clear();
+
+            Console.Wait(param);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Demonstartion der Fritz-Box API (7590)
+
+![NET](https://img.shields.io/badge/NET-10-green.svg)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+![VS2026](https://img.shields.io/badge/Visual%20Studio-2026-white.svg)
+![Version](https://img.shields.io/badge/Version-1.0.2026.0-yellow.svg)
+
+## Projekt 
+Das Demoprojekt dient zum experimentieren mit der Fritzbox. Ziel soll sein, über die AVM API verschiedene Actoren auszulesen.
+
+## Hinweis
+Der Source ist soll auch einfache Art und Weise die Funktionen eines Features zeigen. Der Source ist so geschrieben, das so wenig wie möglich zusätzliche NuGet-Pakete benötigt werden.
+
+## Beispielsource
+
+> Beschreibung
+
+```csharp
+```
+
+```xml
+```
+
+```json
+```
+
+# Versionshistorie
+![Version](https://img.shields.io/badge/Version-1.0.2026.0-yellow.svg)
+- Erste Version
